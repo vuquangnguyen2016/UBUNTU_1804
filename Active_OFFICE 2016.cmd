4de59ec9b0c0c0e1f259c327ce9c566c692d6117
Nguyen Vu Quang <36990033+vuquangnguyen2016@users.noreply.github.com>
2018-10-25 00:18:04 +0700

    Add files via upload

---
+++ Active_OFFICE 2016.cmd
@@ -0,0 +1,5 @@
+cd C:\Program Files\Microsoft Office\Office16
+cscript OSPP.VBS /inpkey:XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99
+cscript OSPP.VBS /sethst:kms.digiboy.ir
+cscript OSPP.VBS /act
+cscript OSPP.VBS /dstatus
